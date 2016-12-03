ce84443f6d2e09b1647c432a57b94a6829849efb
Steve <sabbajax@gmail.com>
2016-12-02 20:04:22 -0500

    Added CSS and javascript files
    Still trying to get base functionality working

--- world.mapper.js
+++ world.mapper.js
@@ -4,19 +4,35 @@ var WorldMapper = function () {
     
     init = function() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
+          center: {lat: 41.319776, lng: -81.626790},
+          zoom: 12
         });    
     }
     
     findLocation = function() {
-        alert('Looking now...' + apiKey);
+        var address = document.getElementById('address').value;
+        alert('Attempting to find ' + address);
+        codeAddress(address);
     }
     
     viewLocation = function() {
         alert('Getting image...');
     }
     
+    function codeAddress(address) {
+        
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == 'OK') 
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
     return {
         init: init,
         findLocation: findLocation,
