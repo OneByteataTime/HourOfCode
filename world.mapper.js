f1578e0e139aaeab800c60d23bd5686a3661d7b0
Steve Jackson <sabbajax@gmail.com>
2016-12-02 22:10:11 -0500

    Show map and show street view working - now to refactor
    Got functionality working - this can be the starting point

--- world.mapper.js
+++ world.mapper.js
@@ -14,18 +14,37 @@ var WorldMapper = function () {
     
     findLocation = function() {
         var address = document.getElementById('address').value;
-        //alert('Attempting to find ' + address);
         codeAddress(address);
     }
     
     viewLocation = function() {
-        alert('Getting image...');
+        var address = document.getElementById('address').value;
+        geocodeAddress(address);
     }
+    
+    function geocodeAddress(address) {
+        geocoder.geocode( { 'address': address}, function(results, status) {
+          if (status == 'OK') { 
+            var position = results[0].geometry.location;
+            
+            var myPlace = {lat: position.lat(), lng: position.lng()};
+            var panorama = new google.maps.StreetViewPanorama(
+            document.getElementById('street-view'),
+            {
+              position: myPlace,
+              pov: {heading: 165, pitch: 0},
+              zoom: 1
+            });
+          } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+          }
+        });   
+        }
     
     function codeAddress(address) {
         
     geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == 'OK') {
+      if (status == 'OK') { 
         map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
             map: map,
